800f0f03e89938f2f99be382c236689385364fd1
loki <growingdever@gmail.com>
2015-06-08 10:04:51 +0900

    로그인 화면에서 소켓으로 메세지 전송까지 확인.

---
+++ Assets/Scripts/SignIn/SceneControllerSignIn.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Boomlagoon.JSON;
+
+public class SceneControllerSignIn : SceneController {
+
+	public UIInput InputID;
+	public UIInput InputPassword;
+
+
+	public void OnClickSignIn() {
+		JSONObject json = new JSONObject ();
+		json.Add ("target", 1);
+		json.Add ("id", InputID.value);
+		json.Add ("password", InputPassword.value);
+
+		SocketWrapper.Instance.WriteSocket (json.ToString());
+	}
+
+	void Update() {
+		if (SocketWrapper.Instance.messageQueue.Count > 0) {
+			string result = SocketWrapper.Instance.Pop ();
+			print (result);
+		}
+	}
+
+}
---
+++ Assets/Scripts/SocketWrapper.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+public class SocketWrapper : MonoBehaviour
+{
+	private static SocketWrapper instance;
+	private static GameObject container;
+
+	public static SocketWrapper Instance 
+	{  
+		get {
+			if (!instance) {  
+				container = new GameObject ();  
+				container.name = "SocketWrapper";  
+				instance = container.AddComponent (typeof(SocketWrapper)) as SocketWrapper;  
+			}  
+			return instance;  
+		}
+	}
+	
+	TcpClient mySocket;
+	NetworkStream theStream;
+	StreamWriter theWriter;
+	StreamReader theReader;
+	String Host = "127.0.0.1";
+	Int32 Port = 10101;
+	internal Boolean socketReady = false;
+
+	public LinkedList<string> messageQueue {
+		get;
+		private set;
+	}
+
+	void Awake ()
+	{
+		SetUpSocket ();
+
+		messageQueue = new LinkedList<string> ();
+	}
+
+	void Update ()
+	{
+		if (socketReady && theStream.DataAvailable) {
+			string read = ReadSocket ();
+			messageQueue.AddLast(read);
+		}
+	}
+
+	public string Pop() {
+		string first = messageQueue.First.Value;
+		messageQueue.RemoveFirst ();
+		return first;
+	}
+
+	void SetUpSocket ()
+	{
+		try {
+			mySocket = new TcpClient (Host, Port);
+			theStream = mySocket.GetStream ();
+			theWriter = new StreamWriter (theStream);
+			theReader = new StreamReader (theStream);
+			socketReady = true;
+		} catch (Exception e) {
+			Debug.LogError ("Socket error: " + e);
+		}
+	}
+
+	public void WriteSocket (string theLine)
+	{
+		if (!socketReady) {
+			return;
+		}
+
+		string foo = theLine + "\r\n";
+		theWriter.Write (foo);
+		theWriter.Flush ();
+	}
+
+	public String ReadSocket ()
+	{
+		if (!socketReady) {
+			return "";
+		}
+		if (theStream.DataAvailable) {
+			return theReader.ReadLine ();
+		}
+
+		return "";
+	}
+
+	public void CloseSocket ()
+	{
+		if (!socketReady) {
+			return;
+		}
+
+		theWriter.Close ();
+		theReader.Close ();
+		mySocket.Close ();
+		socketReady = false;
+	}
+}
